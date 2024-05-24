35076663cdf6a49e7017d49ace07fbbf3c74dd00
Gunwoo Kim <gunwoolouie@gmail.com>
2024-05-24 06:00:16 -0700

    File clean up.

--- client/src/Components/AddButton.js
+++ client/src/Components/AddButton.js
@@ -6,18 +6,18 @@ import AddIcon from '@mui/icons-material/Add';
 
 const AddButton = ({ onClick, style }) => {
 
-    const theme = useTheme();
+  const theme = useTheme();
 
-    return (
-        <Fab 
-            color={theme.palette.base.main} 
-            aria-label="add" 
-            style={style} 
-            onClick={onClick}
-        >
-            <AddIcon/>
-        </Fab>
-    )
+  return (
+    <Fab
+      color={theme.palette.base.main}
+      aria-label="add"
+      style={style}
+      onClick={onClick}
+    >
+      <AddIcon />
+    </Fab>
+  )
 }
 
 
